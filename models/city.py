55c49f62b8ca2f5ab421577cdcc146886ec1f3f2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 14:34:21 -0500

    create-city

--- models/city.py
+++ models/city.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 import os
 
-HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object): 
     """ The city class, contains state ID and name """
