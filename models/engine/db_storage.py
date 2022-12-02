c71c3ff86d06adc7d714b535fd999b47e2b6ffe7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 11:10:58 -0500

    update-db-storagev2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,17 +63,26 @@ class DBStorage:
 
     def delete(self, obj=None):
         """ Deletes tables of DB"""
-        if obj:
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
         """ Create all tables of DB """
         from models.base_model import Base
         from sqlalchemy.orm import sessionmaker, scoped_session
+        """from models.user import User
+        from models.place import Place"""
+        from models.state import State
+        from models.city import City
+        f"""rom models.amenity import Amenity
+        from models.review import Review"""
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        session_factory = sessionmaker(bind=self.__engine,
+                                        expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+        
 
         def close(self):
             Session.close(self.__session)
