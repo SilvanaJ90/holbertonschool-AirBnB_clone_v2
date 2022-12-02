cc46b3b03f691c754ea46a658af6f242513cd1d4
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 11:39:34 -0500

    update-db-storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -56,20 +56,20 @@ class DBStorage:
         return ret_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Doc"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Doc"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Doc"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """ Doc"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
