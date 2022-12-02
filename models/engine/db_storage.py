46303fe8a99d0e93fe5a2dc8cfb6b672d1dd5542
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 11:30:02 -0500

    update-db-storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
-""" Doc """
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import Session
-"""from models.user import User"""
-"""from models.place import Place"""
+""" """
+
+from sqlalchemy import create_engine
+from os import getenv
+from models.base_model import Base
+from models.user import User
 from models.state import State
 from models.city import City
-"""from models.amenity import Amenity"""
-"""from models.review import Review"""
-import os
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from sqlalchemy.orm import Session
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
-classes = {
-            """'User': User, 'Place': Place, 'Amenity': Amenity, 'Review': Review,"""
-            'State': State, 'City': City
-            }
 
 class DBStorage:
-    """ Doc """
-
+    """  """
     __engine = None
     __session = None
 
     def __init__(self):
-        user = os.getenv('HBNB_MYSQL_USER')
-        pwd = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine("mysql+mysqldb://{:s}:{:s}@{:s}/{:s}".format(
-                                    user, pwd, host, db), pool_pre_ping=True)
+        """ """
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{:s}:{:s}@{:s}/{:s}'.format(
+            HBNB_MYSQL_USER,
+            HBNB_MYSQL_PWD,
+            HBNB_MYSQL_HOST,
+            HBNB_MYSQL_DB
+        ), pool_pre_ping=True)
 
-        metaData = MetaData()
-        if os.getenv('HBNB_ENV') == 'test':
-            metaData.drop_all()
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         self.__session = Session(self.__engine)
@@ -82,7 +85,24 @@ class DBStorage:
         Session = scoped_session(session_factory)
         self.__session = Session()
 
-        
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
         def close(self):
             Session.close(self.__session)
