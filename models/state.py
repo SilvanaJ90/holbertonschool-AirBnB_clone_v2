99a026899ab238f2642cc25fa3473483bd557b16
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 14:48:39 -0500

    update-state

--- models/state.py
+++ models/state.py
@@ -18,9 +18,15 @@ class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     else:
         name = ""
 
-        @property
-        def cities(self):
-            from models.city import City
-            from models import storage
-            city_obj = storage.all(City)
-            return [city for city in city_obj.values() if city.state_id == self.id]
+    @property
+    def cities(self):
+        """
+
+        """
+        from models import storage
+        result = []
+        cities = storage.all(City).values()
+        for city in cities:
+            if self.id == city.state_id:
+                result.append(city)
+        return(result)
