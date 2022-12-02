500517225e2bbd45930dd0b7d0d4d738426cd0ba
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 11:04:23 -0500

    update-db-storagev2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,3 +81,6 @@ class DBStorage:
                                         expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+        def close(self):
+            Session.close(self.__session)
