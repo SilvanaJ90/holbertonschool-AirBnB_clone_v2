6e5e197f5e9acb2628ca4efad53906d722dcb352
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 14:58:55 -0500

    update-replace-city-update-test

--- models/state.py
+++ models/state.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
+import os
 
 
-class State(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")
-                                == "db") else object):
+HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
+
+
+class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object): 
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if HBNB_TYPE_STORAGE == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="delete")
-
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """
-        Getter attribute that returns the list of City
-        instances with state_id equals to the current State.id
-        """
-        from models import storage
-        result = []
-        cities = storage.all(City).values()
-        for city in cities:
-            if self.id == city.state_id:
-                result.append(city)
-        return(result)
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+            city_obj = storage.all(City)
+            return [city for city in city_obj.values() if city.state_id == self.id]
