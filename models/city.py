632e1e5a80ef2deb6c642b774670383583822fb5
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 14:57:13 -0500

    update-replace-city-update-test

--- models/city.py
+++ models/city.py
@@ -2,18 +2,23 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-import os
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
-class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object): 
+class City(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")
+                               == "db") else object):
     """ The city class, contains state ID and name """
-    if HBNB_TYPE_STORAGE == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'),
-                        nullable=False)
-        
+        places = relationship(
+            "Place",
+            cascade="all",
+            backref=backref("cities", cascade="all"),
+            passive_deletes=True
+        )
     else:
         state_id = ""
         name = ""
