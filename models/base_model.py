71753dc9cba89f5afeeedcc16f92522e79833387
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-02 19:12:13 -0500

    update-all

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
         storage.new(self)
         if kwargs:
             if 'updated_at' in kwargs.keys():
