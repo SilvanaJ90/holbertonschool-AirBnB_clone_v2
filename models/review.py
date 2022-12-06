50c5fbde72e79f165161b6cea18b3acc036bea00
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 17:33:07 -0500

    update-reviuw

--- models/review.py
+++ models/review.py
@@ -15,8 +15,3 @@ class Review(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
-
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
