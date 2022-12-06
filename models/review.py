437902fe17184bc31769874acc71a354fe50b75b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 17:03:46 -0500

    update-reviuw

--- models/review.py
+++ models/review.py
@@ -14,9 +14,9 @@ class Review(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     """ Review classto store review information """
     if HBNB_TYPE_STORAGE == 'db':
         __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('place.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
     else:
         place_id = ""
         user_id = ""
