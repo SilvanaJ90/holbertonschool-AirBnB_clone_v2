f02c8ebdabaff54f7e40ce791fb823bb53645764
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 14:28:38 -0500

    user

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -15,6 +15,7 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship(
         "Place relationship",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
+        cascade="all,delete",
+        backref=backref("user", cascade="all,delete"),
+        passive_deletes=True,
+        single_parent=True
