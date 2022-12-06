c2c1a7d4937434b2e791b9a2d4ef51b57f328dd1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 13:44:53 -0500

    user

--- models/user.py
+++ models/user.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This module defines a class User"""
+from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
-import os
-
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    if HBNB_TYPE_STORAGE == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+class User(BaseModel, Base):
+    '''User class'''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
+    reviews = relationship(
+        "Review",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
