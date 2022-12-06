51b6aff00ee3c9b7cf5ad22dafb188f91a3016e1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 17:57:13 -0500

    update-reviuw

--- models/city.py
+++ models/city.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+import os
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel, Base):
-    '''User class'''
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="cities", cascade="delete")
+class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+    """ The city class, contains state ID and name """
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        state_id = ""
+        name = ""
