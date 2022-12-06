3c5cad621ab924e698a9919ef8a8476aad098515
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 14:53:55 -0500

    user

--- models/user.py
+++ models/user.py
@@ -13,10 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship(
-        "Place relationship",
-        cascade="all,delete",
-        backref=backref("user", cascade="all,delete"),
-        passive_deletes=True,
-        single_parent=True
-        )
+    places = relationship("Place", backref="user", cascade="delete")
