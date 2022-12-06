1009e815fc9590d8889f1d42f6b9028822e568bb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 18:42:03 -0500

    create-hello-hbnb-in flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
-# save this as app.py
-from flask import Flask, request
-from markupsafe import escape
+#!/usr/bin/python3
+from flask import Flask
+"""Write a script that starts a Flask web application:"""
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
