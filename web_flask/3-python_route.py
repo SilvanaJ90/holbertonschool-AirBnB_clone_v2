2a52574fc9c27535dafea28655074627026f8689
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 23:30:49 -0500

    update -3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,10 +21,10 @@ def cisfun(text):
     return 'C {}'.format(text.replace("_", " "))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    return("Python {}".format(text.replace("_", " ")))
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
