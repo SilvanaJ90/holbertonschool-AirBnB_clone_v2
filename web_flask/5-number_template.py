fc86bd8eeff6b80027552793d5738fdde2b77158
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 23:56:44 -0500

    update -5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """ display hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """ C is fun"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """The default value of text is “is cool”"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ display “n is a number” only if n is an integer"""
+    return("{} is a number".format(n))
+
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_tem():
+    return render_template('5-number.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
