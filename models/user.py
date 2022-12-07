535ffb5c427ffbc2e583d33ecc30f533af510981
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 20:45:32 -0500

    reverse-user

--- models/user.py
+++ models/user.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import Base
-from models.base_model import BaseModel
+""" User Module for HBNB project """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
+import os
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel, Base):
-    '''User class'''
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="delete")
+class User(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+    """ State class """
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            "Place",
+            cascade="all,delete",
+            backref=backref("user", cascade="all,delete"),
+            passive_deletes=True,
+            single_parent=True
+        )
+        reviews = relationship(
+            "Review",
+            cascade="all,delete",
+            backref=backref("user", cascade="all,delete"),
+            passive_deletes=True,
+            single_parent=True
+        )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
