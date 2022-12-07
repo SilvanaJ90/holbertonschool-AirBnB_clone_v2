5f8198bfd64854f3ccfb24309c590a867e232136
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-07 00:32:01 -0500

    update

--- models/city.py
+++ models/city.py
@@ -3,13 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy import  relationship
+import os
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class City(BaseModel, Base):
+class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     """The city class, contains state ID and name"""
-
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
-
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ""
+        name = ""
