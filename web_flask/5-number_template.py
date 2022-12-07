67bf2cf7b48b352522e56403d18aaaf51634aa7f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-07 00:27:58 -0500

    update -5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ display HTML if n is int"""
-    return render_template('5-number.html', n=n)
+    return(render_template('5-number.html', n=n))
 
 
 if __name__ == '__main__':
