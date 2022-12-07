55edb9b1262e49ddf15fdb228fbd6c0128c0c0a6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 22:19:50 -0500

    place

--- models/city.py
+++ models/city.py
@@ -12,8 +12,8 @@ class City(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     """The city class, contains state ID and name"""
     if HBNB_TYPE_STORAGE == 'db':
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship("Place", backref="cities", cascade="delete")
     else:
         state_id = ""
