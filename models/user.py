b6e466f0a712b8b298196e4569c23ca5e71b45da
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-06 22:25:39 -0500

    place

--- models/user.py
+++ models/user.py
@@ -3,30 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
-
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+class User(BaseModel, Base):
     """user class"""
-    if HBNB_TYPE_STORAGE == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="delete")
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="delete")
         
-        """reviews = relationship(
+    """reviews = relationship(
             "Review",
             cascade="all,delete",
             backref=backref("user", cascade="all,delete"),
             passive_deletes=True,
             single_parent=True
         )"""
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
