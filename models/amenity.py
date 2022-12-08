2514c34489222a81ac0b7ac41849375c8ed14990
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-08 13:54:30 -0500

    pp8-style

--- models/amenity.py
+++ models/amenity.py
@@ -13,6 +13,6 @@ class Amenity(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     if HBNB_TYPE_STORAGE == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
- 
+
     else:
         name = ""
