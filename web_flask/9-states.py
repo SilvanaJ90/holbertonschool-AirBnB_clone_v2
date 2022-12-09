1b296376a092959a610b78f6f2dc2c3f5fa916a2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-09 14:47:20 -0500

    update

---
+++ web_flask/9-states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ du must use storage for fetching data from the storage """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(exc):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
