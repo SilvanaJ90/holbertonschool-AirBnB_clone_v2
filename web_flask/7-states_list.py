fdbfb6ba209b987a2740fd3823acaf67afaf561a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-09 11:13:26 -0500

    update-8-states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """Write a script that starts a Flask web application:"""
 from models import storage
 from flask import Flask, render_template
+from models.state import State
 
 app = Flask(__name__)
 
