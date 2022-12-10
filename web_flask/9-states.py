af13f36c382368378f21522722467f2a13bbd00b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-09 23:44:45 -0500

    task10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,21 +7,14 @@ from models.state import State
 
 app = Flask(__name__)
 
-
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """ du must use storage for fetching data from the storage """
-    states = storage.all(State).values()
-    return render_template('9-states.html', states=states)
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def cities_list(state_id=None):
+@app.route('/states', strict_slashes=False)
+def states_cities(state_id=None):
     """ du must use storage for fetching data from the storage """
     states = storage.all(State).values()
     if state_id is not None:
         state_id = 'State.' + state_id
-    return render_template('9-states.html', state_id=state_id)
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
